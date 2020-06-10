b1ade12f89801ca1e9d7a31d1de2a507f7941da0
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-10 15:38:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+  let output = [];
+  for (let i = 0; i < arr.length; i++) {
+    output.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+  return output;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
