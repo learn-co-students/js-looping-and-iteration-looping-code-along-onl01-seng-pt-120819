8fc6da07a8e0ba22d9066bd81e11cff1c3c7b281
asif-kamal <asif.kamal1489@gmail.com>
2020-06-19 10:26:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-// Code your solutions in this file
-let countdown = 10;
-while (countdown >= 0) {
-  console.log(countdown++);
+function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
 }
