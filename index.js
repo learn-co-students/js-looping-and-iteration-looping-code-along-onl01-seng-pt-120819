82440c1c1ef5d737abf31b25299e60c45af19de0
8ceg6 <8ceg6@comcast.net>
2020-06-20 09:58:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const people = ["Ada", "Brendan", "Ali"];
+
+function writeCards(people,event) {let thanks = [];
+    for (let i = 0; i < people.length; i++) {
+     thanks.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`)
+    };
+    return thanks
+}
+writeCards(people,event);
+
+
+
+function countDown(){
+let num = 10
+while (num >= 0){
+    console.log(num--);
+} 
+}
+countDown()
